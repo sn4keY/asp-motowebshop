d927d4c78ff986b634e2f0e2b79a25b6cc6ed7ec
Norbert Neudert <norbertneudert@gmail.com>
2019-11-14 16:48:21 +0100

    Removed unneccessary function calls

--- MotoWebShop/MotoWebShop/Startup.cs
+++ MotoWebShop/MotoWebShop/Startup.cs
@@ -79,26 +79,16 @@ namespace MotoWebShop
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseDatabaseErrorPage();
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseCookiePolicy();
 
             app.UseAuthentication();
-
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
+            app.UseMvc();
         }
     }
 }
