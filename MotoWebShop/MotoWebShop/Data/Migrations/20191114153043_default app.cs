59043d26188c9671c77d2bde8127fb6d22d21326
Norbert Neudert <norbertneudert@gmail.com>
2019-11-14 16:37:45 +0100

    Added "default app" migration

---
+++ MotoWebShop/MotoWebShop/Data/Migrations/20191114153043_default app.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MotoWebShop.Data.Migrations
+{
+    public partial class defaultapp : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
