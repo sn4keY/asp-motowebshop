a5909b744e0cd25b416dbb4b4ec5031edb8e66d8
Norbert Neudert <norbertneudert@gmail.com>
2019-11-15 10:28:21 +0100

    Added Manufacturer class into Common

---
+++ MotoWebShop/MotoWebShop.Common/Manufacturer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MotoWebShop.Common
+{
+    public class Manufacturer
+    {
+        public int Id { get; set; }
+
+        public int Name { get; set; }
+    }
+}
