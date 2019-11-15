2e2faa061e74acff098aabc426f389e26338f692
Norbert Neudert <norbertneudert@gmail.com>
2019-11-15 10:28:09 +0100

    Added Item class into Common

---
+++ MotoWebShop/MotoWebShop.Common/Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MotoWebShop.Common
+{
+    public class Item
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public int Price { get; set; }
+    }
+}
