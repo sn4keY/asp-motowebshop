faa7939ab7e20f9d101e4ddf10374488271c4e84
Gábor Laczik <gabor.laczik@hotmail.com>
2019-11-15 10:20:42 +0100

    Recreated MotoWebShop.Common
    Create Category class

---
+++ MotoWebShop/MotoWebShop.Common/Category.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MotoWebShop.Common
+{
+    public class Category
+    {
+        int Id { get; set; }
+        string Name { get; set; }
+    }
+}
