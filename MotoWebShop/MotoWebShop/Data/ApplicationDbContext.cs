06f68d9075149f5e543965e4b837bb8b5765526b
Norbert Neudert <norbertneudert@gmail.com>
2019-11-17 20:42:29 +0100

    Added tables to ApplicationDbContext

--- MotoWebShop/MotoWebShop/Data/ApplicationDbContext.cs
+++ MotoWebShop/MotoWebShop/Data/ApplicationDbContext.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using MotoWebShop.Common;
 
 namespace MotoWebShop.Data
 {
@@ -12,5 +14,26 @@ namespace MotoWebShop.Data
             : base(options)
         {
         }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            #region "Seed Data"
+
+            builder.Entity<IdentityRole>().HasData(
+                new { Id = "1", Name = "Admin", NormalizedName = "ADMIN" },
+                new { Id = "2", Name = "Customer", NormalizedName = "CUSTOMER" }
+            );
+
+            #endregion
+        }
+
+        // DD27
+
+        public DbSet<Manufacturer> Manufacturers { get; set; }
+        public DbSet<Model> Models { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Item> Items { get; set; }
     }
 }
