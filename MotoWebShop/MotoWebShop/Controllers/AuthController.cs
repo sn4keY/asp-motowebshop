78c5fdec60f62cfdc96d0df8a33a43e314bc0795
Norbert Neudert <norbertneudert@gmail.com>
2019-11-17 20:41:21 +0100

    Added AuthController

---
+++ MotoWebShop/MotoWebShop/Controllers/AuthController.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using MotoWebShop.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace MotoWebShop.Controllers
+{
+    [Route("[controller]")]
+    public class AuthController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly IConfiguration _configuration;
+
+        public AuthController(UserManager<IdentityUser> userManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _configuration = configuration;
+        }
+
+        [Route("Register")] // /Register
+        [HttpPost]
+        public async Task<ActionResult> InsertUser([FromBody] RegisterViewModel model)
+        {
+            var user = new IdentityUser
+            {
+                Email = model.Email,
+                UserName = model.Email,
+                SecurityStamp = Guid.NewGuid().ToString()
+            };
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                await _userManager.AddToRoleAsync(user, "Customer");
+            }
+            return Ok(new { Username = user.UserName });
+        }
+
+        [Route("Login")] // /Login
+        [HttpPost]
+        public async Task<ActionResult> Login([FromBody] LoginViewModel model)
+        { 
+            var user = await _userManager.FindByNameAsync(model.Username);
+            if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
+            {
+                var claim = new[] {
+                    new Claim(JwtRegisteredClaimNames.Sub, user.UserName)
+                };
+                var signinKey = new SymmetricSecurityKey(
+                  Encoding.UTF8.GetBytes(_configuration["Jwt:SigningKey"]));
+
+                int expiryInMinutes = Convert.ToInt32(_configuration["Jwt:ExpiryInMinutes"]);
+
+                var token = new JwtSecurityToken(
+                  issuer: _configuration["Jwt:Site"],
+                  audience: _configuration["Jwt:Site"],
+                  claims: claim,
+                  expires: DateTime.Now.AddMinutes(60),
+                  signingCredentials: new SigningCredentials(signinKey, SecurityAlgorithms.HmacSha256)
+                );
+                ;
+                return Ok(
+                  new
+                  {
+                      token = new JwtSecurityTokenHandler().WriteToken(token),
+                      expiration = token.ValidTo
+                  });
+            }
+            return Unauthorized();
+        }
+
+        [Route("AdminLogin")] // /AdminLogin
+        [HttpPost]
+        public async Task<ActionResult> AdminLogin(LoginViewModel model)
+        {
+            var user = await _userManager.FindByNameAsync(model.Username);
+            var roles = await _userManager.GetRolesAsync(user);
+            if (user != null && await _userManager.CheckPasswordAsync(user, model.Password) && roles.Contains("Admin"))
+            {
+                var claim = new[] {
+                    new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+                    new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", "Admin")
+                };
+                var signinKey = new SymmetricSecurityKey(
+                  Encoding.UTF8.GetBytes(_configuration["Jwt:SigningKey"]));
+
+                int expiryInMinutes = Convert.ToInt32(_configuration["Jwt:ExpiryInMinutes"]);
+
+                var token = new JwtSecurityToken(
+                  issuer: _configuration["Jwt:Site"],
+                  audience: _configuration["Jwt:Site"],
+                  claims: claim,
+                  expires: DateTime.Now.AddMinutes(60),
+                  signingCredentials: new SigningCredentials(signinKey, SecurityAlgorithms.HmacSha256)
+                );
+                return Ok(
+                  new
+                  {
+                      token = new JwtSecurityTokenHandler().WriteToken(token),
+                      expiration = token.ValidTo
+                  });
+            }
+            return Unauthorized();
+        }
+    }
+}
